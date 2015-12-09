4fb90f652a37286f7d5a42b7ca32389aad38efb5
Oles Onyshchak <oles1719@gmail.com>
2015-12-09 18:02:42 +0200

    add navigation bar

--- web/src/main/webapp/app/app.js
+++ web/src/main/webapp/app/app.js
@@ -1,2 +1,12 @@
-angular.module('app',['ngRoute','spring-security-csrf-token-interceptor','ui.router']).config(function($routeProvider){
-});
+angular.module('app',['ngRoute','spring-security-csrf-token-interceptor','ui.router']).config(  ['$stateProvider',function($stateProvider){
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    template: 'app/welcome/template/home.html'
+                })
+                .state('contact', {
+                    url: '/contact',
+                    template: 'app/welcome/template/contact.html'
+                });
+
+        }]);
