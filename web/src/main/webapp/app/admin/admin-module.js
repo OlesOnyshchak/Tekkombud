e58dca26856b013e797d1106a6c043a95999f36b
Oles Onyshchak <oles1719@gmail.com>
2015-12-14 12:32:50 +0200

    add routing to admin page

--- web/src/main/webapp/app/admin/admin-module.js
+++ web/src/main/webapp/app/admin/admin-module.js
@@ -1,2 +1,18 @@
-angular.module('admin',['ngRoute','spring-security-csrf-token-interceptor']).config(function($routeProvider){
-});
+var admin = angular.module('admin',['ngRoute','spring-security-csrf-token-interceptor', 'ui.router','ui.bootstrap']);
+admin.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('otherwise', {
+            url:'/',
+            templateUrl: 'app/admin/index.html'
+        })
+        .state('cv', {
+            url:'/cv',
+            templateUrl: 'app/admin/template/cv.html'
+        })
+        .state('message', {
+            url:'/message',
+            templateUrl: 'app/admin/template/message.html'
+        })
+}]);
