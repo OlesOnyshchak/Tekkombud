9304a4f8259812b70c3e3251045a019ef02a22a2
Oles Onyshchak <oles1719@gmail.com>
2016-01-04 14:46:21 +0200

    add to english and ukrainian json file

--- web/src/main/webapp/app/app.js
+++ web/src/main/webapp/app/app.js
@@ -1,23 +1,54 @@
 var module = angular.module('app', ['spring-security-csrf-token-interceptor', 'ui.router', 'ngMap', 'ui.bootstrap', 'angularUtils.directives.dirPagination', 'pascalprecht.translate']);
 module.config(['$translateProvider', '$stateProvider', '$urlRouterProvider', function ($translateProvider, $stateProvider, $urlRouterProvider) {
 
-    $translateProvider.translations('en', {
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'app/assets/i18n/welcome-',
+        suffix: '.json'
+    });
+
+    $translateProvider.preferredLanguage('ukr');
+
+    /*$translateProvider.translations('en', {
         'Home': 'Home',
         'Projects': 'Projects',
         'Offers': 'Offers',
         'Contact': 'Contact',
-        'LANG': "en"
-    });
+     'lang': "en",
+     'Tekkombud': "Tekkombud",
+     'Sign in':"Sign in",
+     'username':"username",
+     'password':"password",
+     'Our offers':"Our offers",
+     'Id':"Id",
+     'Name':"Name",
+     'Description':"Description",
+     'Skills':"Skills",
+     'Salary':"Salary",
+     'Manipulation':"Manipulation"
+
+
+     });
 
     $translateProvider.translations('ukr', {
         'Home': 'Домашня',
         'Projects': 'Проекти',
         'Offers': 'Вакансії',
         'Contact': 'Контакти',
-        'LANG': "укр"
+     'lang': "укр",
+     'Tekkombud': "Теккомбуд",
+     'Sign in':"Увійти",
+     'username':"логін",
+     'password':"пароль",
+     'Our offers':"Наші вакансії",
+     'Id':"Порядковий номер",
+     'Name':"Ім'я",
+     'Description':"Опис",
+     'Skills':"Навички",
+     'Salary':"Зарплата",
+     'Manipulation':"Дії"
     });
 
-    $translateProvider.preferredLanguage('en');
+     $translateProvider.preferredLanguage('en');*/
 
     $urlRouterProvider.otherwise('/');
 
