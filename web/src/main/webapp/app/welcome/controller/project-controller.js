687e9770afd4a185466c72c450379b5ba8b20f70
Oles Onyshchak <oles1719@gmail.com>
2016-01-12 12:33:34 +0200

    add project page style

--- web/src/main/webapp/app/welcome/controller/project-controller.js
+++ web/src/main/webapp/app/welcome/controller/project-controller.js
@@ -5,33 +5,27 @@ angular.module('app')
             [
                 {
                     "ImageID": 1,
-                    "Title": "Tulips",
-                    "Summary": "This is summary of Tulips",
+                    "Title": "Railway station ",
+                    "Summary": "first building",
                     "Path": "app/welcome/util/photo/1.jpg"
                 },
                 {
                     "ImageID": 2,
-                    "Title": "Jellyfish",
-                    "Summary": "This is something about Jellyfish",
+                    "Title": "Modern house  ",
+                    "Summary": "second building",
                     "Path": "app/welcome/util/photo/2.jpg"
                 },
                 {
                     "ImageID": 3,
-                    "Title": "Lighthouse",
-                    "Summary": "Summary of Lighthouse",
+                    "Title": "Reconstruction ",
+                    "Summary": "third building",
                     "Path": "app/welcome/util/photo/3.jpg"
                 },
                 {
                     "ImageID": 4,
-                    "Title": "Penguins",
-                    "Summary": "Something about Penguins",
-                    "Path": "app/welcome/util/photo/1.jpg"
-                },
-                {
-                    "ImageID": 5,
-                    "Title": "Penguins",
-                    "Summary": "Something about Penguins",
-                    "Path": "app/welcome/util/photo/2.jpg"
+                    "Title": "Detached house",
+                    "Summary": "fourth building",
+                    "Path": "app/welcome/util/photo/4.jpg"
                 }
             ]
 
